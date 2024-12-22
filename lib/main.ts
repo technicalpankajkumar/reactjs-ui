7d80ef803bb1a8bfb20805d21f972f24b19a42c6
technicalpankajkumar <technicalpankajkumar@gmail.com>
2024-12-22 13:49:43 +0530

    added tailwind css

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,2 @@
-
+import "./index.css"
 export * from './components/button/Button'
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
+// import { Button } from '../lib/components/button/Button'
 import { Button } from 'libraricals-ui'
+import "../dist/libraricals-ui.css"
 
 
 function App() {
 
   return (
     <>
-     <Button value="Hello" isLoading size='small'/>
+     <Button value="Button" size='small'/>
      <Button value="Hello" isLoading size='medium'/>
      <Button value="Hello" isLoading size='large'/>
     </>
